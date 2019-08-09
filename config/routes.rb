0df59afc1f3d1bf91d7a38c59d90205245479af3
github username <github email address>
2019-08-09 10:19:50 -0500

    adds root route and view, a SessionsController, a WelcomeController. Comments out omniauth google routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Routes for Google authentication
- get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
- get ‘auth/failure’, to: redirect(‘/’)
+
+  root to: 'welcome#home'
+  #get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
+  #get ‘auth/failure’, to: redirect(‘/’)
 end
