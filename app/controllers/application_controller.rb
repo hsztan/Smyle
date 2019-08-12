18709d7514aaa7175f54bb5a3f0928e40eb8ad10
github username <github email address>
2019-08-11 19:46:03 -0500

    adds hearo logged in and current hearo helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
 
+  
+  def require_logged_in
+    return redirect_to(controller: 'welcome', action: 'home') unless smyler_logged_in || hearo_logged_in?
+  end
+
+  #smyler helper methods
   def current_smyler
     @smyler = (Smyler.find_by(id: session[:smyler_id]) || Smyler.new)
   end
@@ -8,7 +14,13 @@ class ApplicationController < ActionController::Base
     current_smyler.id != nil
   end
 
-  def require_logged_in
-    return redirect_to(controller: 'welcome', action: 'home') unless smyler_logged_in?
+  #hearo helper methods
+  def current_smyler
+    @hearo = (Hearo.find_by(id: session[:hearo_id]) || Hearo.new)
   end
+
+  def hearo_logged_in?
+    current_hearo.id != nil
+  end
+
 end
