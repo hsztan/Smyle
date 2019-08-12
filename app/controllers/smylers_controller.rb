9cc67d2d05a1340c95eaf27ddfc605c2f5d1a31b
github username <github email address>
2019-08-11 19:40:35 -0500

    facebook oath signup works

--- app/controllers/smylers_controller.rb
+++ app/controllers/smylers_controller.rb
@@ -9,9 +9,10 @@ class SmylersController < ApplicationController
         u.name = auth['info']['name']
         u.email = auth['info']['email']
         u.image = auth['info']['image']
+        u.password = "FACEBOOK"  #TODO Find a secure method to use bcrypt in this case
       end
       session[:smyler_id] = @smyler.id
-      redirect_to: @smyler
+      redirect_to smyler_path(@smyler)
     else
       #create manually and login
     end
