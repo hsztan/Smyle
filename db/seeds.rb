e6308f9ceaafa61fb7939b6532229a1a8c2e62c3
github username <github email address>
2019-08-13 12:22:03 -0500

    adds specialty instances to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,8 @@
 Gender.create(name: "Male")
 Gender.create(name: "Female")
 Gender.create(name: "Non-Binary")
+Specialty.create(name: "Behavioral Therapist")
+Specialty.create(name: "Psychoanalist")
+Specialty.create(name: "PTSD")
+Specialty.create(name: "Loneliness")
+Specialty.create(name: "Alientist")
