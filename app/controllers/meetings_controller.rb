79df88b775ff9e75862e5ace40b68cd6a100bfc2
github username <github email address>
2019-08-14 17:28:29 -0500

    functioning meeting index view with controller action

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,15 @@
 class MeetingsController < ApplicationController
 
+  def index
+    if smyler_logged_in?
+      @meetings = current_smyler.meetings
+    elsif hearo_logged_in?
+      @meetings = current_hearo.meetings
+    else
+      return redirect_to '/'
+    end
+  end
+
   def show
     @meeting = Meeting.find_by(id: params[:id])
   end
