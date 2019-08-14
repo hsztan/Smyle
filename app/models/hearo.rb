264c668ccdeb829a508dd8a21fb813ca770b6f94
github username <github email address>
2019-08-14 10:22:03 -0500

    adds custom writer specialty_attributes in hearo model in order to find or create and not create the same specialty name

--- app/models/hearo.rb
+++ app/models/hearo.rb
@@ -4,7 +4,11 @@ class Hearo < ApplicationRecord
   has_many :meetings
   belongs_to :gender
   belongs_to :specialty
-  accepts_nested_attributes_for :specialty
+  #accepts_nested_attributes_for :specialty
   has_many :smylers, through: :meetings
   has_secure_password
+
+  def specialty_attributes=(attributes)
+    self.specialty = Specialty.find_or_create_by(name: attributes[:name])
+  end
 end
