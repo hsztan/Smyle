ed03227dfa55b6931c68251e48412649d66da003
github username <github email address>
2019-08-15 11:50:32 -0500

    adds method full_name to Hearos instances

--- app/models/hearo.rb
+++ app/models/hearo.rb
@@ -13,6 +13,14 @@ class Hearo < ApplicationRecord
     self.specialty = Specialty.find_or_create_by(name: attributes[:name])
   end
 
+  def full_name
+    if self.middle_name
+      self.first_name + ' ' + self.middle_name + ' ' + self.last_name
+    else
+      self.first_name + ' ' + self.last_name
+    end
+  end
+
   def self.online
     self.all.select {|h| h.status.name == 'Online'}
   end
